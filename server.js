b7649a740dd9b0e264af670adaee0b7e410f0385
Samuel <slauric1@gmail.com>
2017-07-20 19:18:23 -0400

    changes

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 3000;
 
 let app = express();
 
-hbs.registerPartials(__dirname + '/views/partials')
+hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
 
 app.use((req,res, next)=>{
